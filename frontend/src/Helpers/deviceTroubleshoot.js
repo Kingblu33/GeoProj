2e4e943db108b16df757a58de02ccfd9c8be7e0c
Kingblu33 <demmarblue@gmail.com>
2024-04-30 20:15:16 -0400

    rearranged objs

---
+++ frontend/src/Helpers/deviceTroubleshoot.js
@@ -0,0 +1,37 @@
+export const SamsaraData = [
+    {
+        image: "https://images.ctfassets.net/bx9krvy0u3sx/7ia7qpeK6TeGaAvtoIE6U8/5b7365b8c75ae8b315d3096e66f678c0/fleet-manager-4-vg34.jpg",
+        description: "Samsara offers innovative fleet management and IoT solutions.",
+        troubleshoot: "Troubleshoot common issues with Samsara devices using the provided guide."
+    },
+    {
+        image: "https://images.ctfassets.net/bx9krvy0u3sx/7ia7qpeK6TeGaAvtoIE6U8/5b7365b8c75ae8b315d3096e66f678c0/fleet-manager-4-vg34.jpg",
+        description: "Samsara's platform helps optimize fleet performance and safety measures.",
+        troubleshoot: "Learn how to troubleshoot Samsara hardware and software issues."
+    },
+];
+
+export const ZonarData = [
+    {
+        image: "https://i.ebayimg.com/images/g/EXwAAOSwuXllejN3/s-l1200.webp",
+        description: "Zonar provides smart fleet management solutions.",
+        troubleshoot: "Refer to the Zonar troubleshooting guide for assistance."
+    },
+    {
+        image: "https://i.ebayimg.com/images/g/EXwAAOSwuXllejN3/s-l1200.webp",
+        description: "Zonar provides smart fleet management solutions.",
+        troubleshoot: "Refer to the Zonar troubleshooting guide for assistance."
+    }
+];
+
+export const GeotabData = [
+    {
+        image: "https://www.geotab.com/CMS-Media-production/Product/GO/geotab-go9-hero.jpg",
+        description: "Geotab is a vehicle tracking and fleet management platform."
+    },
+    {
+        image: "https://www.geotab.com/CMS-Media-production/Product/GO/geotab-go9-hero.jpg",
+        description: "Geotab offers advanced telematics solutions for businesses."
+    },
+];
+
