3b4dc961702b9145755a2c4aca0fd9b9e2c535bf
Kingblu33 <demmarblue@gmail.com>
2024-04-30 20:30:44 -0400

    Testing mono

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -145,25 +145,25 @@ const Home = () => {
                     <div className="w-full md:w-1/2 py-10 px-5 md:px-10">
                         <form onSubmit={submit}>
                             <div className="text-center mb-10">
-                                <h1 className="font-bold text-3xl text-gray-900">
+                                <h1 className="font-bold font-mono text-3xl text-gray-900">
                                     Device Check
                                 </h1>
                             </div>
                             <div className="mb-4">
-                                <label className="block text-xs font-semibold mb-1" htmlFor="provider">
+                                <label className=" font-mono block text-xs font-semibold mb-1" htmlFor="provider">
                                     Choose a provider
                                 </label>
                                 <select
                                     id="provider"
-                                    className="block w-full border border-gray-600 rounded-md px-3 py-2 bg-white text-black"
+                                    className="font-mono block w-full border border-gray-600 rounded-md px-3 py-2 bg-white text-black"
                                 >
                                     <option value="Geotab">Geotab</option>
                                     <option value="Geotab">Samsara</option>
                                     <option value="Geotab">Zonar</option>
                                 </select>
                             </div>
-                            <div className="mb-4">
-                                <label className="block text-xs font-semibold mb-1" htmlFor="serialNumber">
+                            <div className="mb-3">
+                                <label className="block text-xs font-semibold mb-1 font-mono" htmlFor="serialNumber">
                                     Enter Serial Number (Don't include dashes)
                                 </label>
                                 <input
@@ -175,7 +175,7 @@ const Home = () => {
                                 />
                             </div>
                             <div className="mb-4">
-                                <label className="block text-xs font-semibold mb-1" htmlFor="location">
+                                <label className="font-mono block text-xs font-semibold mb-1" htmlFor="location">
                                     Enter Location (xxxx-xx)
                                 </label>
                                 <input
@@ -188,7 +188,7 @@ const Home = () => {
                             </div>
                             <button
                                 type="submit"
-                                className={`w-full py-2 rounded-md focus:outline-none ${serialNumber.length < 5 || serialNumber.length > 12
+                                className={`font-mono w-full py-2 rounded-md focus:outline-none ${serialNumber.length < 5 || serialNumber.length > 12
                                     ? "bg-gray-500 text-gray-300 cursor-not-allowed"
                                     : "bg-blue-500 text-white hover:bg-blue-600 focus:bg-blue-600"
                                     
