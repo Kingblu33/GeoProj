0c46040228ad32291c85ec5fda73bbd064e96ca0
Kingblu33 <Demmarblue@gmail.com>
2024-07-18 11:35:33 -0400

    si

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Result from "./pages/Result"
-import Help from "./pages/Help";
 import Content from "./pages/Content";
+import LandingPage from "./pages/LandingPage";
 
 const App = () =>{
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element ={<Home/>}/>
+        <Route path="/" element ={<LandingPage/>}/>
         <Route path="/Home" element ={<Home/>}/>
         <Route path="/results" element ={<Result/>}/>
-        <Route path = "/help" element = {<Help/>}/>
-        <Route path = "/content" element = {<Content/>}/>
+        <Route path = "/help" element = {<Content/>}/>
+        <Route path = "/LandingPage" element = {<LandingPage/>}/>
       </Routes>
     </BrowserRouter>
   )
